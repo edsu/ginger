279a80f64e539d37205c3008681ef50287fcb0be
Daniel Krech <eikeon@eikeon.com>
2013-07-09 17:27:01 -0400

    Inlined patch.js script.

--- gingerweb/bootstrap.sh
+++ gingerweb/bootstrap.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 pushd bower_components/bootstrap/
-node ../../patch.js
+
+read -d '' PATCH <<-"EOF"
+fs = require('fs');
+var data = JSON.parse(fs.readFileSync("package.json"));
+data["devDependencies"]["recess"] = "1.1.8";
+fs.writeFileSync("package.json", JSON.stringify(data, null, 2));
+EOF
+node -e "${PATCH}"
+
 npm install
 make
 make bootstrap
--- gingerweb/patch.js
+++
@@ -1,5 +0,0 @@
-fs = require('fs');
- 
-var data = JSON.parse(fs.readFileSync("package.json"));
-data["devDependencies"]["recess"] = "1.1.8"
-fs.writeFileSync("package.json", JSON.stringify(data, null, 2))

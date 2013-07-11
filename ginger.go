9a4eecd1ec5871728c578e5b3b5aa71a8a0c2c12
Daniel Krech <eikeon@eikeon.com>
2013-07-11 08:36:56 -0400

    Added Queue, Add, Fetcher, and Persister.

--- ginger.go
+++ ginger.go
@@ -1,25 +1,100 @@
 package ginger
 
 import (
-	"net/url"
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 type FetchRequest struct {
-	URL url.URL
+	URL *url.URL
+}
+
+func (req *FetchRequest) Fetch() *FetchResponse {
+	response, err := http.Get(req.URL.String())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &FetchResponse{response}
 }
 
 type FetchResponse struct {
 	Response *http.Response
 }
 
+type Queue struct {
+	messages chan string
+}
+
+func (q *Queue) SendMessage(message interface{}) error {
+	b, err := json.Marshal(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+	q.messages <- string(b)
+	return nil
+}
+
+func (q *Queue) ReceiveMessage(i interface{}) error {
+	message := <-q.messages
+	if message == "" {
+		return errors.New("empty")
+	}
+	if err := json.Unmarshal([]byte(message), &i); err != nil {
+		//log.Fatal(err) TODO: a FetchResponse we can round trip
+		log.Println("ERROR:", err)
+	}
+	return nil
+}
+
+type Result struct {
+	StatusCode    int
+	ContentLength int64
+}
+type Results map[string]Result
+
 type Ginger struct {
+	requests  Queue
+	responses Queue
+	Results   Results
+}
+
+func NewGinger() *Ginger {
+	return &Ginger{Queue{make(chan string, 100)}, Queue{make(chan string, 100)}, make(Results)}
 }
 
 func (g *Ginger) Greeting() string {
 	return "Hello, world!"
 }
 
-func Fetch(req *FetchRequest) *FetchResponse {
-	return &FetchResponse{&http.Response{}}
+func (g *Ginger) Add(url *url.URL) {
+	fr := FetchRequest{url}
+	g.requests.SendMessage(fr)
+}
+
+func (g *Ginger) Fetcher() {
+	for {
+		var request FetchRequest
+		err := g.requests.ReceiveMessage(&request)
+		if err != nil {
+			log.Println("Done fetching")
+			break
+		}
+		response := request.Fetch()
+		g.responses.SendMessage(response)
+	}
+}
+
+func (g *Ginger) Persister() {
+	for {
+		var response FetchResponse
+		err := g.responses.ReceiveMessage(&response)
+		if err != nil {
+			log.Println("done persisting")
+			break
+		}
+		g.Results[response.Response.Request.URL.String()] = Result{response.Response.StatusCode, response.Response.ContentLength}
+	}
 }
--- ginger_test.go
+++ ginger_test.go
@@ -1,14 +1,35 @@
 package ginger_test
 
 import (
-	"net/http"
+	//"net/http"
+	"net/url"
 	"testing"
+	"time"
 
 	"github.com/bmizerany/assert"
 	"github.com/eikeon/ginger"
 )
 
 func TestFetch(t *testing.T) {
-	req := &ginger.FetchRequest{}
-	assert.Equal(t, ginger.Fetch(req), &ginger.FetchResponse{&http.Response{}})
+	u, err := url.Parse("http://www.eikeon.com/")
+	if err != nil {
+		t.Error(err)
+	}
+	req := &ginger.FetchRequest{u}
+	response := req.Fetch()
+	assert.Equal(t, response.Response.StatusCode, 200)
+}
+
+func TestGinger(t *testing.T) {
+	g := ginger.NewGinger()
+	go g.Fetcher()
+	go g.Persister()
+	u, err := url.Parse("http://www.eikeon.com/")
+	if err != nil {
+		t.Error(err)
+	}
+	g.Add(u)
+	time.Sleep(1 * time.Second)
+	_, ok := g.Results["http://www.eikeon.com/"]
+	assert.Equal(t, ok, true)
 }

1812a4a00a44471a9cdba453b547367bc2016f13
Daniel Krech <eikeon@eikeon.com>
2013-07-16 14:17:41 -0400

    Implemented NewWarcReader to fix test.

--- warc/warc.go
+++ warc/warc.go
@@ -4,9 +4,10 @@ import (
 	"io"
 )
 
-func NewWarcReader(reader io.Reader) Reader {
-	panic("TODO")
+func NewWarcReader(reader io.Reader) *Reader {
+	return &Reader{reader}
 }
 
 type Reader struct {
+	reader io.Reader
 }

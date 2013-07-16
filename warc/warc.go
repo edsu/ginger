71c6ef538a5049a072e75d452b2c9d8fce703c35
Daniel Krech <eikeon@eikeon.com>
2013-07-16 14:57:11 -0400

    Added ReadRecord and added reading the version record to the test.

--- warc/warc.go
+++ warc/warc.go
@@ -1,13 +1,32 @@
 package warc
 
 import (
+	"bufio"
+	"errors"
 	"io"
 )
 
+type Record struct {
+	Version string
+}
+
 func NewWarcReader(reader io.Reader) *Reader {
-	return &Reader{reader}
+	return &Reader{bufio.NewReader(reader)}
 }
 
 type Reader struct {
-	reader io.Reader
+	reader *bufio.Reader
+}
+
+func (wr *Reader) ReadRecord() (*Record, error) {
+	r := &Record{}
+	version, isPrefix, err := wr.reader.ReadLine()
+	if isPrefix == true {
+		return nil, errors.New("buffer not big enough to read version line")
+	}
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+	r.Version = string(version)
+	return r, nil
 }
--- warc/warc_test.go
+++ warc/warc_test.go
@@ -1,16 +1,28 @@
 package warc_test
 
 import (
+	"compress/gzip"
 	"os"
 	"testing"
 
+	"github.com/bmizerany/assert"
 	"github.com/eikeon/ginger/warc"
 )
 
 func TestReader(t *testing.T) {
-	if f, err := os.OpenFile("test.warc.gz", os.O_RDONLY, 0666); err == nil {
-		warc.NewWarcReader(f)
-	} else {
+	f, err := os.OpenFile("test.warc.gz", os.O_RDONLY, 0666)
+	if err != nil {
 		t.Error(err)
+		t.FailNow()
 	}
+	gr, err := gzip.NewReader(f)
+	assert.Equal(t, err, nil)
+
+	wr := warc.NewWarcReader(gr)
+	vr, err := wr.ReadRecord()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	assert.Equal(t, vr.Version, "WARC/1.0")
 }

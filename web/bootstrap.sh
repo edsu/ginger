82a8ba8bd1ebbb42960d894ebeffe7cffcc4f843
Daniel Krech <eikeon@eikeon.com>
2013-07-18 15:37:58 -0400

    Removed patch script; switched to bootstrap fork.

--- web/bootstrap.sh
+++ web/bootstrap.sh
@@ -3,15 +3,6 @@
 # this is called from the Makefile
 
 pushd bower_components/bootstrap/
-
-read -d '' PATCH <<-"EOF"
-fs = require('fs');
-var data = JSON.parse(fs.readFileSync("package.json"));
-data["devDependencies"]["recess"] = "1.1.8";
-fs.writeFileSync("package.json", JSON.stringify(data, null, 2));
-EOF
-node -e "${PATCH}"
-
 npm install
 make
 make bootstrap
